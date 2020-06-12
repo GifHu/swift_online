a12d4c408907395a6da1eb47fc87a66471810f0d
yanjingxin <yanjingxin@moqing.com>
2020-06-12 14:55:20 +0800

    new

---
+++ helloWorld/Sources/App/routes.swift
@@ -0,0 +1,28 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "xxxxxxxxxxxxxxxxxxxxx"
+    }
+    
+    router.get("hi") { req in
+        return "adadadmakmda"
+    }
+    
+    router.get("111") { req in
+        return "222222"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
